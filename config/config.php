a5e59c493e88ccb46e9bd4b2d236b6ef45a7e419
Nemolite <g25092011@mail.ru>
2022-05-06 10:04:32 +0300

    connect

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 06.05.2022
+ * Time: 8:58
+ */
+
+$config = [
+    'host' => '127.0.0.1',
+    'db'   => 'rbasa',
+    'user' => 'root',
+    'pass' => 'root',
+];
--- index.php
+++ index.php
@@ -18,3 +18,31 @@ $password = $config['pass'];
 
 #R::setup( 'mysql:host=localhost;dbname=mydatabase','user', 'password' );
 R::setup( $dsn,$username, $password );
+
+// Проверка подключения
+if (R::testConnection()){
+    echo "Есть подключение к базе данных";
+}
+// Замарозка
+R::freeze(true);
+
+// Создание таблицы баз данных
+$user = R::dispense('user');
+
+$user->surname = 'Vushnyakov';
+$user->name = 'Sergey';
+$user->patronymic = 'Valerevich';
+$user->age = 50;
+$user->country = 'Russia';
+
+R::store($user);
+//Извлечение
+$selecet_user = R::load('user',3);
+$h->show($selecet_user);
+
+// Обновление
+$upuser = R::load('user',3);
+$upuser->age = 51;
+R::store($upuser);
+
+R::close();
