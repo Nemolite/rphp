3977296a7de4d9dd59171af875512e390ce88c95
Nemolite <g25092011@mail.ru>
2022-05-06 09:20:45 +0300

    connect

---
+++ Helpers.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 06.05.2022
+ * Time: 9:07
+ */
+
+class Helpers
+{
+     public function show($param){
+         echo "<pre>";
+         print_r($param);
+         echo "</pre>";
+     }
+}
