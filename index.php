c14bde7b51e8b55961f99464425082f555aaee49
Nemolite <g25092011@mail.ru>
2022-05-06 09:18:30 +0300

    connect

--- index.php
+++ index.php
@@ -5,3 +5,16 @@
  * Date: 06.05.2022
  * Time: 8:33
  */
+include "Helpers.php";
+require_once "rb/rb.php";
+require "config/config.php";
+
+$h = new Helpers();
+$h->show($config);
+
+$dsn = 'mysql:host='.$config['host'].';dbname='.$config['db'];
+$username = $config['user'];
+$password = $config['pass'];
+
+#R::setup( 'mysql:host=localhost;dbname=mydatabase','user', 'password' );
+R::setup( $dsn,$username, $password );
